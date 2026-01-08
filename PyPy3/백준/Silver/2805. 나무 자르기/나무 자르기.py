7ea076c66d36d2b68cda83c275cfbf8311245efe
Seunghyun Yoo <ads137485@jbnu.ac.kr>
2026-01-08 22:10:51 +0900

    [Silver II] Title: 나무 자르기, Time: 424 ms, Memory: 258360 KB -BaekjoonHub

--- PyPy3/백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ PyPy3/백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,28 +1,32 @@
 import sys
 input = sys.stdin.readline
 
+# 제한 시간 1초, (1<= N <= 1,000,000  1<= M <= 2,000,000,000)
+## 나무의 길이는 2억 이므로, 자르는 길이를 브루트포스 방식으로 선형적으로 탐색하면 시간초과
+## 이분 탐색 방법으로 탐색하면 시간 안에 탐색
+### 탐색 지점은 트리의 합계가 최소 M이상이 되는 경계
 def main():
-    N, M = map(int, input().split())
-    tree_list = list(map(int, input().split()))
     
-    left, right = 0, max(tree_list)
-    result = 0  # 정답을 저장할 변수
+    N, M = map(int,input().split())
+    tree_list = list(map(int,input().split()))
+    
+    left , right = 0, max(tree_list)
 
     while left <= right:
         target = (left + right) // 2
-        total = 0
+        sum = 0
 
-        # target 높이로 잘랐을 때 가져갈 수 있는 나무 길이
+        # target의 값을 계산
         for tree in tree_list:
-            if tree > target:
-                total += tree - target
+            if target < tree:
+                sum += tree - target
 
-        # 이분 탐색 조건
-        if total >= M:  # 충분히 가져갈 수 있음
+        # tartget 값에 따라 탐색 범위 수정
+        if M <= sum:
             result = target  # 현재 높이를 저장
-            left = target + 1  # 더 높은 높이 시도
-        else:  # 부족함
-            right = target - 1  # 더 낮은 높이 시도
+            left = target + 1
+        elif M > sum:
+            right = target -1
 
     print(result)
 
