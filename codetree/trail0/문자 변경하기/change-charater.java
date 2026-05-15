2da47d7418d07abffe59044fcdf89e23bd3659c7
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-05-15 19:38:58 +0900

    Delete codetree/trail0 directory

--- codetree/trail0/문자 변경하기/change-charater.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Please write your code here.
-        char a = 'C';
-        a = 'T';
-        System.out.println(a);
-    }
-}
