0fc530d2c5ebc287bb80008ddda109bae0e51d82
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-05-16 19:44:18 +0900

    [Passed] 비교 연산 (128ms, 10MB)

--- Java/codetree/trail0/비교 연산/comparison-operator.java
+++ Java/codetree/trail0/비교 연산/comparison-operator.java
@@ -1,46 +1,21 @@
-
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
-        if (A >= B) {
-            System.out.println(1);
-        }
-        else {
-            System.out.println(0);
-        }
-        if (A > B) {
-            System.out.println(1);
-        } else{
-            System.out.println(0);
-        }
-        if (B >= A){
-            System.out.println(1);
-        }
-        else{
-            System.out.println(0);
-        }
-        if (B > A){
-            System.out.println(1);
-        }
-        else{
-            System.out.println(0);
-        }
-        if (A == B){
-            System.out.println(1);
-        }
-        else{
-            System.out.println(0);
-        }
-        if (A != B){
-            System.out.println(1);
-        }
-        else{
-            System.out.println(0);
-        }
+
+        System.out.println(A >= B ? "1" : "0");
+        
+        System.out.println(A > B ? "1" : "0");
+        
+        System.out.println(B >= A ? "1" : "0");
+        
+        System.out.println(B > A ? "1" : "0");
+        
+        System.out.println(A == B ? "1" : "0");
+        
+        System.out.println(A != B ? "1" : "0");
+
     }
 }
-
