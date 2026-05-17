afd762e3f00faafdc5fe016a19f4404a7905c966
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-05-18 01:22:22 +0900

    [Passed] 점수 비교 (121ms, 10MB)

---
+++ Java/codetree/trail0/점수 비교/score-comparison.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int AMath = sc.nextInt();
+        int AEng = sc.nextInt();
+        int BMath = sc.nextInt();
+        int BEng = sc.nextInt();
+
+        if (AMath > BMath && AEng > BEng){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+
+    }
+}
