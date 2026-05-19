9044a76cafa43816dbe9cf00a6af4996fe75c04b
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-05-20 02:04:50 +0900

    [Passed] a로 채워넣기 (116ms, 10MB)

---
+++ Java/codetree/trail0/a로 채워넣기/filling-with-a.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        str = str.substring(0, 1)
+                + "a"
+                + str.substring(2, str.length() - 2)
+                + "a"
+                + str.substring(str.length() - 1);
+
+        System.out.println(str);
+
+
+
+    }
+}
