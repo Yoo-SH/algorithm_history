89b84a1e5816bd7c2b2d4907666ed24f7c4c7770
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-05-22 22:09:46 +0900

    [Passed] 친근하지 않은 수 (134ms, 10MB)

---
+++ Java/codetree/trail0/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = 0;
+
+        for(int i=1; i<=N; i++){
+            if (i % 2 == 0 || i % 3 == 0 || i % 5 ==0){
+                continue;
+            }
+            count += 1;
+        }
+
+        System.out.print(count);
+    }
+}
+
