ce77b939f7e91ad42abbc7df81509d173f1cc5ee
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-05-29 00:32:28 +0900

    [Passed] 따옴표 출력 (97ms, 9MB)

---
+++ Java/codetree/trail1/따옴표 출력/print-quote.java
@@ -0,0 +1,17 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        bw.write("He says \"It's a really simple sentence\".");
+        bw.close();
+    }
+}
+
+
