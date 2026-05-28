cdf40e64b3f0425b49fc62e2206d0ccf7f21f001
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-05-29 00:42:33 +0900

    [Passed] 정수 선언하고 곱 출력 (87ms, 9MB)

---
+++ Java/codetree/trail1/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,21 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        bw.write("26 * 5 = 130");
+        bw.close();
+    }
+}
+
+
+
+
+
