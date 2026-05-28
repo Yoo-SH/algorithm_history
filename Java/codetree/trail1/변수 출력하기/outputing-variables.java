525182070aa838b6fa3f0b39314f883e442ecc9d
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-05-29 00:53:55 +0900

    [Passed] 변수 출력하기 (103ms, 9MB)

---
+++ Java/codetree/trail1/변수 출력하기/outputing-variables.java
@@ -0,0 +1,22 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        int a = 3;
+        String b = "C";
+        bw.write(a + "..." + b);
+        bw.close();
+    }
+}
+
+
+
+
+
