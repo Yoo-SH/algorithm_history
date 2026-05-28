de3a63e7b78600496161d1f95dd37c218ee84756
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-05-29 01:03:01 +0900

    [Passed] 소수점 반올림하기 (96ms, 9MB)

---
+++ Java/codetree/trail1/소수점 반올림하기/rounding-decimal-points.java
@@ -0,0 +1,19 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        double a = 25.352;
+        bw.write(String.format("%.1f", a));
+        bw.close();
+    }
+}
+
+
+
