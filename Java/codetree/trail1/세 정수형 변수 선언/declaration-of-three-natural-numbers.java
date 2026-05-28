108172a2a50c3eb04e3f489b3526fba2e2a9dc83
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-05-29 00:49:05 +0900

    [Passed] 세 정수형 변수 선언 (100ms, 9MB)

---
+++ Java/codetree/trail1/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,20 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        int a = 7;
+        int b = 23;
+        int c =  a+ b;
+        bw.write(a + " + " + b + " = " + c);
+        bw.close();
+    }
+}
+
+
