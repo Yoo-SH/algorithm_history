bc6e31b6ca9b732e18e45faad4e62222652ef0cb
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-01 02:41:21 +0900

    [Passed] 문자 변경하기 (98ms, 9MB)

---
+++ Java/codetree/trail1/문자 변경하기/change-charater.java
@@ -0,0 +1,17 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        char a = 'C';
+        a = 'T';
+        bw.write(a);
+        bw.close();
+    }
+}
