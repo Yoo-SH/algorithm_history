74c14296cea98a76822b3c0d5e339edc61a04da9
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-02 02:46:38 +0900

    [Passed] 입력받아 계산 3 (97ms, 9MB)

---
+++ Java/codetree/trail1/입력받아 계산 3/input-calculate-3.java
@@ -0,0 +1,19 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        int a = Integer.parseInt(br.readLine().trim());
+        int b = Integer.parseInt(br.readLine().trim());
+        bw.write(String.format("%d",a*b));
+        bw.close();
+    }
+}
