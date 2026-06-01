226a5a6aa75515691e056cce3bea96e8931278f9
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-02 03:07:17 +0900

    [Passed] 문자열 순서 바꾸기 (107ms, 9MB)

---
+++ Java/codetree/trail1/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,18 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        String T = br.readLine().trim();
+        String S = br.readLine().trim();
+        bw.write(String.format("%s\n%s",S,T));
+        bw.close();
+    }
+}
