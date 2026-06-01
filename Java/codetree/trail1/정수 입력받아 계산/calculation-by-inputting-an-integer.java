298a4637731e4fde2f3561a53e71954c0f27a852
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-02 02:29:20 +0900

    [Passed] 정수 입력받아 계산 (95ms, 9MB)

---
+++ Java/codetree/trail1/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,16 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        int a = Integer.parseInt(br.readLine().trim());
+        bw.write(String.format("%d", a*2+3));
+        bw.close();
+    }
+}
