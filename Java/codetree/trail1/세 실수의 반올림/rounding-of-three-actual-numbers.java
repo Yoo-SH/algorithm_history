3e5d063a6a49f39ae2d8bbaaffcbb5c314fb57b0
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-02 02:51:32 +0900

    [Passed] 세 실수의 반올림 (106ms, 9MB)

---
+++ Java/codetree/trail1/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,20 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        Double a = Double.parseDouble(br.readLine().trim());
+        Double b = Double.parseDouble(br.readLine().trim());
+        Double c = Double.parseDouble(br.readLine().trim());
+        bw.write(String.format("%.3f\n%.3f\n%.3f",a,b,c));
+        bw.close();
+    }
+}
