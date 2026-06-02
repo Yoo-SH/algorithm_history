26e139241c58c92ac06608f4f18f4ec50230ad3e
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-02 22:48:15 +0900

    [Passed] 간단한 사칙연산 (100ms, 9MB)

---
+++ Java/codetree/trail1/간단한 사칙연산/simple-arithmetic-operation.java
@@ -0,0 +1,22 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        bw.write(String.format("%d\n",A+B));
+        bw.write(String.format("%d\n",A-B));
+        bw.write(String.format("%d\n",A/B));
+        bw.write(String.format("%d",A%B));
+        bw.close();
+    }
+}
