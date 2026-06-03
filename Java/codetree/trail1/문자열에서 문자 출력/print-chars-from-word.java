58554e446eeb3e4cd3cd025f88a25b12d4f736ca
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-03 23:15:38 +0900

    [Passed] 문자열에서 문자 출력 (92ms, 9MB)

---
+++ Java/codetree/trail1/문자열에서 문자 출력/print-chars-from-word.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        String str = br.readLine();
+
+        for(int i=0; i<str.length();i++){
+            bw.write(str.charAt(i)+"\n");
+        }
+
+        bw.close();
+    }
+}
