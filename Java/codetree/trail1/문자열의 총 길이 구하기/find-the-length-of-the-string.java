e39be6600160db3cd4459958fb42247718b846a3
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-03 22:02:26 +0900

    [Passed] 문자열의 총 길이 구하기 (100ms, 9MB)

---
+++ Java/codetree/trail1/문자열의 총 길이 구하기/find-the-length-of-the-string.java
@@ -0,0 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int len =0;
+
+        while (st.hasMoreTokens()){
+            len += st.nextToken().length();
+        }
+
+        bw.write(String.format("%d",len));
+        bw.close();
+    }
+}
