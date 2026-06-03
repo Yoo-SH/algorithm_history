1a5ef57d0f71f20bc54176e880bf702262c153c7
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-03 23:47:16 +0900

    [Passed] 짝수 번째만 거꾸로 출력 (90ms, 9MB)

---
+++ Java/codetree/trail1/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        String str = br.readLine();
+
+        for(int i=str.length()-1;i>=0;i--){
+            if(i % 2 != 0){
+                bw.write(str.charAt(i));
+            }
+        }
+
+
+        bw.close();
+    }
+}
