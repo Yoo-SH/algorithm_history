57d8bff0914dc87b0c9fc37bef2c8c692b584c43
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-08 15:59:52 +0900

    [Passed] e 제거하기 (91ms, 9MB)

---
+++ Java/codetree/trail1/e 제거하기/e-to-remove.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+
+        int idx = str.indexOf("e");
+        str = str.substring(0,idx) + str.substring(idx+1);
+        bw.write(str);
+        bw.close();
+    }
+}
