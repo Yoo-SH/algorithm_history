80927450d9864321472e62fa27edac7a8a1ae832
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-08 10:18:22 +0900

    [Passed] 부분문자열 위치 구하기 (94ms, 9MB)

---
+++ Java/codetree/trail1/부분문자열 위치 구하기/find-location-of-substring.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        String target = br.readLine();
+        int idx = str.indexOf(target);
+        bw.write(String.format("%d",idx));
+
+        bw.close();
+
+    }
+}
