4c48ce4d3ef2318cc2f7c2c4df5d6adc78ce4312
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-08 16:49:29 +0900

    [Passed] 문자열 밀기 (95ms, 9MB)

---
+++ Java/codetree/trail1/문자열 밀기/push-char.java
@@ -0,0 +1,22 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        int strLen = str.length();
+
+        str = str.substring(1,strLen) + str.substring(0,1);
+
+        bw.write(str);
+        bw.close();
+    }
+}
