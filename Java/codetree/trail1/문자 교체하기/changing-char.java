ac651655feba5d36722e0e0d4d868f0dc9abde74
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-08 10:43:23 +0900

    [Passed] 문자 교체하기 (96ms, 9MB)

---
+++ Java/codetree/trail1/문자 교체하기/changing-char.java
@@ -0,0 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str1 = st.nextToken();
+        String str2 = st.nextToken();
+
+        String token = str1.substring(0,2);
+        str2 = token + str2.substring(2);
+
+        bw.write(str2);
+        bw.close();
+    }
+}
