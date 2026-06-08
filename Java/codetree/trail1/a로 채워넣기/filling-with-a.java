0ae7bd384ed525baa5884f02886b8af0b710782a
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-08 10:35:50 +0900

    [Passed] a로 채워넣기 (89ms, 9MB)

---
+++ Java/codetree/trail1/a로 채워넣기/filling-with-a.java
@@ -0,0 +1,21 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+
+        str = str.substring(0,1) + "a" + str.substring(2,str.length()-2) + "a" + str.substring(str.length()-1);
+        bw.write(str);
+        bw.close();
+
+    }
+}
