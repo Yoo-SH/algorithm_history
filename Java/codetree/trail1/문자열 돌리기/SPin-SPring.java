a218500502b292705e9176ad5fbc1e4e86c45851
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-08 16:53:37 +0900

    [Passed] 문자열 돌리기 (91ms, 9MB)

---
+++ Java/codetree/trail1/문자열 돌리기/SPin-SPring.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        int strLen = str.length();
+
+        for (int i=0;i<strLen+1;i++){
+            bw.write(str+"\n");
+            str = str.substring(strLen-1,strLen) + str.substring(0,strLen-1) ;
+        }
+        bw.close();
+    }
+}
