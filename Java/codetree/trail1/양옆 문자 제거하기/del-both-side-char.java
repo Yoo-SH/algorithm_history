a7466ca1dac372cfb39d9d512333fa7b747d19ca
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-08 15:45:58 +0900

    [Passed] 양옆 문자 제거하기 (93ms, 9MB)

---
+++ Java/codetree/trail1/양옆 문자 제거하기/del-both-side-char.java
@@ -0,0 +1,20 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        str = str.substring(0,1) + str.substring(2,str.length()-2) + str.substring(str.length()-1);
+
+        bw.write(str);
+        bw.close();
+    }
+}
