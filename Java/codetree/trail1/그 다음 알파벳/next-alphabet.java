6cc19044d6b8ea1f26b315cde47cc64236ba9bb6
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-11 02:16:34 +0900

    [Passed] 그 다음 알파벳 (108ms, 9MB)

---
+++ Java/codetree/trail1/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,24 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char ch = br.readLine().charAt(0);
+        if (ch =='z'){
+            bw.write('a');
+        } else{
+            bw.write(ch +1);
+        }
+        bw.close();
+    }
+}
