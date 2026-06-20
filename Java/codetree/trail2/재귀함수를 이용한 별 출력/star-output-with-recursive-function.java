acd6653aec20038f7ef88bda6e19758b3c10b471
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-20 20:21:49 +0900

    [Passed] 재귀함수를 이용한 별 출력 (99ms, 9MB)

---
+++ Java/codetree/trail2/재귀함수를 이용한 별 출력/star-output-with-recursive-function.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static void printStartRecursive(int n) throws IOException {
+        if (n ==0) return;
+
+        printStartRecursive(n-1);
+        for (int i =0;i<n;i++){
+            bw.write("*");
+        }
+        bw.write("\n");
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        printStartRecursive(N);
+        bw.close();
+    }
+}
+
