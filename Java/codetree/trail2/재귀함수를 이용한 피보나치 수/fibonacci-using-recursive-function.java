fce3321d13fc5e6df75ca8e274a962fe2e94fd4b
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-21 12:21:13 +0900

    [Passed] 재귀함수를 이용한 피보나치 수 (95ms, 9MB)

---
+++ Java/codetree/trail2/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,27 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int fibonachiRecursive(int n) throws IOException {
+        if (n==1 || n ==2) return 1;
+
+        return fibonachiRecursive(n-1) + fibonachiRecursive(n-2);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(fibonachiRecursive(N)));
+        bw.close();
+    }
+}
+
