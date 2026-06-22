a5a7ff756b0b2680c79a6a03e8ec1011f1a2ec36
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-22 21:59:06 +0900

    [Passed] 재귀함수를 이용한 3N + 1 수열 (91ms, 9MB)

---
+++ Java/codetree/trail2/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int count = 0;
+
+    static int calculRecursive(int n) throws IOException {
+        if (n == 1) return 1;
+
+        count++;
+        if (n % 2 == 0) return calculRecursive(n/2);
+        else return calculRecursive(n*3 +1);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        calculRecursive(N);
+        bw.write(String.valueOf(count));
+        bw.close();
+    }
+}
+
