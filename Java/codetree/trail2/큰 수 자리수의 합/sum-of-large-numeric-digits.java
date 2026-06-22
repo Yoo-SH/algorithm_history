28d89fffcb4923c474410f8a3639a34e0ac0a40c
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-22 21:49:57 +0900

    [Passed] 큰 수 자리수의 합 (100ms, 9MB)

---
+++ Java/codetree/trail2/큰 수 자리수의 합/sum-of-large-numeric-digits.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    static int calculRecursive(int n) {
+        if (n <10) return n;
+
+        return calculRecursive(n/10) + (n % 10);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(calculRecursive(a*b*c)));
+        bw.close();
+    }
+}
+
