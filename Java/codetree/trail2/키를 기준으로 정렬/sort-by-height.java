71cf1bb684d2a52495be3ad115130cf5da26748c
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-24 12:00:54 +0900

    [Passed] 키를 기준으로 정렬 (254ms, 15MB)

---
+++ Java/codetree/trail2/키를 기준으로 정렬/sort-by-height.java
@@ -0,0 +1,50 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        Student [] arrStudent = new Student[n];
+
+        for(int i=0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int height = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            arrStudent[i] = new Student(name,height,weight);
+        }
+
+        Arrays.sort(arrStudent, (a,b)-> a.height - b.height);
+
+        for(int i=0;i<n;i++){
+            bw.write(String.format("%s %d %d\n",arrStudent[i].name,arrStudent[i].height,arrStudent[i].weight));
+        }
+        bw.close();
+
+    }
+}
+
+
+class Student{
+    String name;
+    int height;
+    int weight;
+
+    Student(String name, int height, int weight){
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+}
