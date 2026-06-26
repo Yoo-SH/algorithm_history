afc8a98eb083d1839c0dabc3dee9624411f1608d
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-26 12:27:14 +0900

    [Passed] 십진수로 변환하기 (93ms, 9MB)

---
+++ Java/codetree/trail2/십진수로 변환하기/convert-to-decimal.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        String bin = br.readLine().trim();
+        int result = Integer.parseInt(bin,2);
+        bw.write(String.valueOf(result));
+
+        bw.close();
+
+    }
+}
