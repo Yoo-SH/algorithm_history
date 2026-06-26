b8d3f9765bcac40d65d007181c3e0902a077003a
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-26 12:21:55 +0900

    [Passed] 2진수로 변환하기 (95ms, 9MB)

---
+++ Java/codetree/trail2/2진수로 변환하기/convert-to-binary.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        bw.write(Integer.toBinaryString(N));
+
+        bw.close();
+
+    }
+}
