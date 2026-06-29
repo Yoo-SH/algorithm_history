1f7958c2d8d2d78dc34e40b334dca7ca0ffd977e
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-29 23:42:47 +0900

    [Passed] 연속되는 수 2 (103ms, 10MB)

---
+++ Java/codetree/trail2/연속되는 수 2/continuous-number2.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int arr [] = new int[n];
+
+        //초기화
+        for(int i = 0;i<n;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        // 연속하여 동일한 숫자가 나오는 횟수를 구함
+        int countMax =0;
+        int count =0;
+        for (int i=0;i<n;i++){
+            if(i ==0 || arr[i-1] != arr[i]) count =0;
+            else count++;
+
+            countMax = Math.max(countMax,count);
+        }
+
+
+        bw.write(String.valueOf(countMax+1));
+        bw.close();
+    }
+}
