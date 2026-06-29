044b5068675fe680754752107c4d3bd0128f4c8d
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-06-30 00:22:09 +0900

    [Passed] 연속되는 수 4 (93ms, 9MB)

---
+++ Java/codetree/trail2/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int [] arr = new int[n];
+
+        for (int i =0;i<n;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int countMax = 1;
+        int count = 1;
+
+        for (int i=0;i<n;i++){
+            if (i == 0 || arr[i-1] >=  arr[i]) count =1; // 증가하지 않는다면
+            else count++;
+
+            countMax = Math.max(countMax,count);
+        }
+
+        bw.write(String.valueOf(countMax));
+        bw.close();
+    }
+}
