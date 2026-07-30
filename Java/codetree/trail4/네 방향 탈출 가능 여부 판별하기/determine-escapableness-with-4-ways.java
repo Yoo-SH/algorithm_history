6b52bd3e1163dc8c867925eac815647fb84915c4
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-07-30 14:40:40 +0900

    [Passed] 네 방향 탈출 가능 여부 판별하기 (97ms, 9MB)

---
+++ Java/codetree/trail4/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,67 @@
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Deque;
+import java.util.Queue;
+import java.util.List;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+
+// 제한 시간 1초, BFS탐색
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int [][] matrix = new int[N][M];
+        boolean [][] isVisited = new boolean[N][M];
+        Queue<int []> queue = new ArrayDeque<>();
+        queue.offer(new int[]{0,0});
+        boolean isCanOff = false;
+
+        // 상, 하, 좌, 우
+        int [] dy = new int[]{1,-1,0,0};
+        int [] dx = new int[]{0,0,-1,1};
+
+        // matrix 초기화
+        for(int y=0;y<N;y++){
+            st = new StringTokenizer(br.readLine());
+            for(int x=0;x<M;x++){
+                matrix[y][x] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        while (!queue.isEmpty()){
+            int []cur = queue.poll();
+            int cy = cur[0], cx = cur[1];
+
+            if(cy == N-1 && cx == M-1) {
+                isCanOff =true;
+                break;
+            }
+
+            for(int i =0;i<4;i++){
+                int ny = cy +dy[i], nx = cx + dx[i];
+
+                // 유효한 범위인 경우
+                if(0<=ny && ny <N && 0<= nx && nx <M && matrix[ny][nx] ==1 && !isVisited[ny][nx]){
+                    isVisited[ny][nx] = true;
+                    queue.offer(new int[]{ny,nx});
+                }
+
+            }
+        }
+
+        bw.write(isCanOff ? "1" : "0");
+        bw.flush();
+        bw.close();
+    }
+}
