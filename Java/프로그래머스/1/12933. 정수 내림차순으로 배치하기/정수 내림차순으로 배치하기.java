75a2ede62345e55345d5dc742090124cddbc9fea
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-08-20 11:47:32 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 1.55 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        StringBuilder sb = new StringBuilder();
+        
+        String str = String.valueOf(n);
+        int arr [] = new int [str.length()];
+
+        for(int i= 0; i<str.length();i++){
+            arr[i] = str.charAt(i) - '0';
+        }
+        
+        Arrays.sort(arr);
+        for(int i=arr.length-1;i>=0;i--){
+            sb.append(arr[i]);
+        }
+        
+        
+        long answer = Long.parseLong(sb.toString());
+        return answer;
+    }
+}
