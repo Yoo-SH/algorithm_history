06fed21487d76833d392537e3e4f74d5978e4716
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-08-23 01:51:17 +0900

    [Passed] 가장 오래 걸리는 학생 2 (675ms, 30MB)

---
+++ Java/codetree/trail5/가장 오래 걸리는 학생 2/longest-student-2.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+
+        List<int[]>[] graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph[v].add(new int[]{u, w}); 
+        }
+
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[n] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        pq.add(new int[]{n, 0});
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int node = cur[0], d = cur[1];
+            if (d > dist[node]) continue; // 이미 더 짧은 경로를 찾은 뒤라 무시
+            for (int[] e : graph[node]) {
+                int next = e[0], cost = d + e[1];
+                if (cost < dist[next]) {
+                    dist[next] = cost;
+                    pq.add(new int[]{next, cost});
+                }
+            }
+        }
+        int maxValue = -1;
+        for (int i = 1; i <= n; i++) {
+            maxValue = Math.max(maxValue, dist[i]);
+        }
+        System.out.print(maxValue);
+    }
+}
