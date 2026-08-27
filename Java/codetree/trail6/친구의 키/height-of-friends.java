3af3a51cc2600a4b99dda2202db05868efcbeef4
seunghyeon Yu <ads137485@jbnu.ac.kr>
2026-08-28 01:35:24 +0900

    [Passed] 친구의 키 (460ms, 44MB)

---
+++ Java/codetree/trail6/친구의 키/height-of-friends.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        int [] indegree = new int [n+1];
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<=n;i++)
+            graph.add(new ArrayList<>());
+
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(b);
+            indegree[b] +=1;
+        }
+
+        for(int i=1;i<=n;i++){
+            if(indegree[i] == 0) queue.add(i);
+        }
+
+        while(!queue.isEmpty()){
+            int x = queue.poll();
+
+            sb.append(x).append(" ");
+
+            for (int y : graph.get(x)) {      // graph.get(x)
+            if (--indegree[y] == 0) queue.add(y);
+        }
+
+        }
+
+        System.out.print(sb);
+
+    }
+}
